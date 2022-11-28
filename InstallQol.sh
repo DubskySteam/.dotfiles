6e13a3ff2c9f4d6d6e2d9edfc3fc1e71c68d7b6f
Dubsky <dubcmsky@protonmail.com>
2022-11-28 10:57:16 +0100

    added install script

---
+++ InstallQol.sh
@@ -0,0 +1,16 @@
+echo '#########################################'
+echo 'SCRIPT TESTED ON DEBIAN 11 + UBUNTU 20.04'
+echo '#########################################'
+echo GitHub::Repo = https://github.com/DubskySteam/dotfiles
+echo Installing "Quality of Life" packages
+
+echo Checking OS
+MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
+OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
+
+echo Need ROOT permissions
+if [ $MYOS = "Ubuntu" ] then
+	if [ $UID -ne 0 ]; then
+		apt-get install screenfetch git neovim curl -y
+	fi
+fi
