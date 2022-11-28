990bbcdb42b8edf471398cf1ff91ec81bf7fdab1
Dubsky <dubcmsky@protonmail.com>
2022-11-28 14:00:59 +0100

    revert mistake

--- Install.sh
+++ Install.sh
@@ -18,5 +18,5 @@ then
         echo "Installing: Languages & Environments"
         apt-get install git neovim python3 gcc g++ openjdk-17-jdk openjdk-17-jre -y
         echo "Creating symlinks"
-        stow $HOME/.dotfiles/bash/
+        stow bash/
 fi
