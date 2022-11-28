2168fb1b10cf4cf51eb28693b158d0c525e556bd
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2022-11-28 15:49:43 +0100

    added arch support for install script

--- Install.sh
+++ Install.sh
@@ -8,7 +8,7 @@ MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
 OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
 
 echo !!! TO CONTINUE YOU NEED ROOT PERMISSIONS !!!
-if [ $MYOS = "Ubuntu" ]
+if [ $MYOS = "Ubuntu" ] || [ $MYOS = "Debian" ] || [ $MYOS = "Kali" ] || [ $MYOS = "Elementary" ]
 then
         echo "Checking for updates"
         apt-get update
@@ -19,6 +19,21 @@ then
         apt-get install git neovim python2 python3 gcc g++ openjdk-17-jdk openjdk-17-jre -y
         curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
         apt install -y nodejs
+        echo "Creating symlinks"
+        stow bash/
+        stow nvim/
+        stow git/
+elif [ $MYOS = "Manjaro" ] || [ $MYOS = "Arch" ]
+then
+        echo "!!! AUR SUPPORT NEED TO BE ACTIVATED!!! "
+        echo "Checking for updates"
+        pacman -Sy
+        pacman -Syu
+        echo "Installing: Qualify of Life tools"
+		pacman -S stow screenfetch git net-tools curl wget -y
+        echo "Installing: Languages & Environments"
+        pacman -S neovim python gcc jre17-openjdk -y
+        yay -S nodejs-lts-gallium
         echo "Creating symlinks"
         stow bash/
         stow nvim/
