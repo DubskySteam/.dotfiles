98fce882c01ab8557784551f19d3b6b9ca1a94fc
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-05-02 05:10:29 +0200

    added Symlink batch file for windows && update .gitconfig

--- ps7/Microsoft.Powershell_profile.ps1
+++ ps7/Microsoft.Powershell_profile.ps1
@@ -1,8 +1,4 @@
-oh-my-posh init pwsh --config "https://gist.githubusercontent.com/DubskySteam/bdc2071ded20eddf7328aa5398bbf02a/raw/e98b956c527cdb4bc3d5a01d8940323451a0a6ff/dubsky_custom.omp.json" | Invoke-Expression
-Import-Module -Name Terminal-Icons
 New-Alias -Name ll -Value ls
 New-Alias -Name g -Value git
 New-Alias -Name vi -Value nvim
 New-Alias -Name vim -Value nvim
-New-Alias -Name gcc -Value x86_64-w64-mingw32-gcc
-New-Alias -Name 'g++' -Value 'x86_64-w64-mingw32-g++'
