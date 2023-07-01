1184f929d436e094a92ecd765bd87b8af08ea420
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2023-07-01 13:14:47 +0200

    update nvim config

---
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,28 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+	'tsserver',
+	'eslint',
+	'rust_analyzer',
+})
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
+
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
---
+++ nvim/.config/nvim/lua/dubsky/init.lua
@@ -0,0 +1,4 @@
+require("dubsky.packer")
+require("dubsky.config")
+require("dubsky.remap")
+print "Config loaded"
