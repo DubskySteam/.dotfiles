f36a498d151a03f049309e1097913bf40c518806
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-16 14:48:15 +0200

    update powershell startup

--- ps7/Microsoft.Powershell_profile.ps1
+++ ps7/Microsoft.Powershell_profile.ps1
@@ -1,4 +1,22 @@
-New-Alias -Name ll -Value ls
-New-Alias -Name g -Value git
-New-Alias -Name vi -Value nvim
-New-Alias -Name vim -Value nvim
+Set-Alias -Name ll Get-ChildItem
+Set-Alias -Name la Get-ChildItem -Force
+Set-Alias -Name g -Value git
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name cdh -Value "cd $HOME"
+
+function Show-ASCIIArt {
+@"
+
+ ______    ___  ____   ___ ___  ____  ____    ____  _     
+|      |  /  _]|    \ |   |   ||    ||    \  /    || |    
+|      | /  [_ |  D  )| _   _ | |  | |  _  ||  o  || |    
+|_|  |_||    _]|    / |  \_/  | |  | |  |  ||     || |___ 
+  |  |  |   [_ |    \ |   |   | |  | |  |  ||  _  ||     |
+  |  |  |     ||  .  \|   |   | |  | |  |  ||  |  ||     |
+  |__|  |_____||__|\_||___|___||____||__|__||__|__||_____|
+                                                          
+
+"@
+}
+Show-ASCIIArt
