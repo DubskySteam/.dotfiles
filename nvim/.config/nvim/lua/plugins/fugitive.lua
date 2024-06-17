3722e25f31194335e790a4acd52b4845af1618f2
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-17 11:18:56 +0100

    added nvim commit shortcut

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", "<cmd>Git status<CR>")
+        vim.keymap.set("n", "<leader>gc", ":Git commit -m ");
         vim.keymap.set("n", "<leader>gp", "<cmd>:Git push<CR>");
         vim.keymap.set("n", "<leader>gd", "<cmd>:Git diff<CR>");
     end
