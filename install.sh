7622cc2b20ba0e74a3ccb6b7d9365af1d9edf733
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-19 01:15:48 +0200

    updated arch install script

--- install.sh
+++ install.sh
@@ -3,45 +3,37 @@
 C_CLEAR='\033[0m'
 C_RED='\033[0;31m'
 C_CYAN='\033[0;36m'
+C_GREEN='\033[0;32m'
 
-echo '###################################'
-echo 'SCRIPT TESTED ON ARCH LINUX 6.6.10'
-echo '###################################'
 echo 'GitHub::Repo = https://github.com/DubskySteam/.dotfiles'
 
 printf "> ${C_CYAN}Updating:${C_CLEAR} Keyring\n"
-sudo pacman -Sy --noconfirm archlinux-keyring manjaro-keyring endeavouros-keyring
+sudo pacman -Sy --noconfirm archlinux-keyring
 
 printf "> ${C_CYAN}Updating:${C_CLEAR} Mirrors\n"
-if [ "$(grep -Fxq 'ID=arch' /etc/os-release)" ]; then
-    sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-elif [ "$(grep -Fxq 'ID=manjaro' /etc/os-release)" ]; then
-    sudo pacman-mirrors --fasttrack && sudo pacman -Syy
-elif [ "$(grep -Fxq 'ID=endeavouros' /etc/os-release)" ]; then
-    sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
-fi
+sudo reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 printf "> ${C_CYAN}Updating:${C_CLEAR} System\n"
 sudo pacman -Syu --noconfirm
 
-printf "> ${C_RED}The script will now install packages. Are you sure you want to do this?\n${C_CLEAR}"
+printf "${C_RED}> The script will now install packages. Are you sure you want to do this?${C_CLEAR}\n"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) echo "alright...."; break;;
-        No ) exit;;
+        Yes ) printf "${C_GREEN}Proceeding with installation...${C_CLEAR}\n"; break;;
+        No ) printf "${C_RED}Installation aborted.${C_CLEAR}\n"; exit;;
     esac
 done
 
-echo "> Installing: Essentials"
+printf "> ${C_CYAN}Installing:${C_CLEAR} Essentials\n"
 sudo pacman -S --noconfirm stow tmux neofetch wget gpgme eza base-devel cmake make wofi ninja gradle
 
-echo "> Installing: QoL Tools"
+printf "> ${C_CYAN}Installing:${C_CLEAR} QoL Tools\n"
 sudo pacman -S --noconfirm mpv nomacs btop
 
-echo "> Installing: Languages & Environments"
-sudo pacman -S --noconfirm git neovim python3 gcc jdk17-openjdk
+printf "> ${C_CYAN}Installing:${C_CLEAR} Languages & Environments\n"
+sudo pacman -S --noconfirm git neovim python gcc jdk17-openjdk
 
-echo "> Creating symlinks"
+printf "> ${C_CYAN}Creating symlinks${C_CLEAR}\n"
 stow nvim
 stow tmux
 stow bash
