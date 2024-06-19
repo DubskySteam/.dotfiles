5b1cb0d9e73fd5e96535a906cb4218a3ddf6ead1
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-19 15:55:44 +0200

    update nvim lsp key mappings

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -4,6 +4,6 @@ return {
         vim.keymap.set("n", "<leader>gs", "<cmd>Git status<CR>")
         vim.keymap.set("n", "<leader>gc", ":Git commit -m ");
         vim.keymap.set("n", "<leader>gp", "<cmd>:Git push<CR>");
-        vim.keymap.set("n", "<leader>gd", "<cmd>:Git diff<CR>");
+        -- vim.keymap.set("n", "<leader>gd", "<cmd>:Git diff<CR>");
     end
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,23 @@ return {
     },
 
     config = function()
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+            callback = function(event)
+                local opts = {buffer = event.buf}
+
+                vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+            end,
+        })
+
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
