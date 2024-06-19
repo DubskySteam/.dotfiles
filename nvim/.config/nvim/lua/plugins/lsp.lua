430f141cd03574a82ff5cbb04a8a88439ffccfe3
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-19 15:47:10 +0200

    fix: autocomplete on enter instead of C-y

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -27,8 +27,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
-                "clangd"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -66,7 +64,7 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<cr>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
