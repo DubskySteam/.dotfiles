8e08a50316ad6ae984924703c6518e6460984e08
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-20 17:02:01 +0200

    nvim: open neotree keybind now toggles

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pp", "<CMD>:Neotree<CR>")
+vim.keymap.set("n", "<leader>pp", "<CMD>:Neotree toggle<CR>")
 vim.keymap.set("n", "<leader>p[", "<CMD>:wincmd p<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
