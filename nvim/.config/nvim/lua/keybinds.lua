3897f383dba994fbd551798907f61aa0e7351f2e
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2024-06-20 17:12:50 +0200

    nvim: fixed editor focus keybind

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,6 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pp", "<CMD>:Neotree toggle<CR>")
-vim.keymap.set("n", "<leader>p[", "<CMD>:wincmd p<CR>")
+
+vim.cmd([[
+function! FocusEditorPane() abort
+    let l:current_win = win_getid()
+    for l:win in range(1, winnr('$'))
+        if l:win != l:current_win && getwinvar(l:win, '&filetype') !=# 'neo-tree'
+            execute l:win . 'wincmd w'
+            return
+        endif
+    endfor
+endfunction
+]])
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':call FocusEditorPane()<CR>', { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
