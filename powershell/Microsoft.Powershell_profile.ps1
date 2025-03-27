3afe8662bf2f1971552529d84a30a84cefd973d9
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-03-27 18:45:11 +0100

    removed ascii art from powershell startup

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -1,3 +1,4 @@
+Clear-Host
 Set-Alias -Name g -Value git
 Set-Alias -Name vi -Value nvim
 Set-Alias -Name vim -Value nvim
@@ -28,19 +29,4 @@ function ll {
 
 fnm env --use-on-cd | Out-String | Invoke-Expression
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\zash.omp.json" | Invoke-Expression
-
-function Show-ASCIIArt {
-@"
-
- ______    ___  ____   ___ ___  ____  ____    ____  _     
-|      |  /  _]|    \ |   |   ||    ||    \  /    || |    
-|      | /  [_ |  D  )| _   _ | |  | |  _  ||  o  || |    
-|_|  |_||    _]|    / |  \_/  | |  | |  |  ||     || |___ 
-  |  |  |   [_ |    \ |   |   | |  | |  |  ||  _  ||     |
-  |  |  |     ||  .  \|   |   | |  | |  |  ||  |  ||     |
-  |__|  |_____||__|\_||___|___||____||__|__||__|__||_____|
-                                                          
-
-"@
-}
-Show-ASCIIArt
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
