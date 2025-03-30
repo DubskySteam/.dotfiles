7967c01ba90288b4604473ee3bdde90482a5bf73
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-03-30 07:37:49 +0100

    added tabline to wezterm for now, needs way more config though

--- wezterm/.config/wezterm/.wezterm.lua
+++ wezterm/.config/wezterm/.wezterm.lua
@@ -1,4 +1,40 @@
 local wezterm = require 'wezterm'
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'Catppuccin Mocha',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { 'hostname' },
+    tabline_b = { 'domain' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'process', padding = 0 }
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { '' },
+    tabline_y = { 'datetime' },
+    tabline_z = { '' },
+  },
+  extensions = {},
+})
 
 local config = {}
 
