b47cfd92e094b1fdebd464d3ba87241e63b4f63e
Dubsky <dubcmsky@proton.me>
2026-08-25 05:12:45 +0200

    hyprland: added hyprlock and different shortcuts

--- hyprland/.config/hypr/hyprland.lua
+++ hyprland/.config/hypr/hyprland.lua
@@ -36,7 +36,7 @@ hl.monitor({
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = "wofi --show drun"
+local menu        = "wofi --show drun --style $HOME/.config/wofi/themes/$(cat $HOME/.cache/waybar-theme 2>/dev/null || echo dark).css"
 
 
 -------------------
@@ -259,13 +259,15 @@ hl.device({
 local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 
 -- Example binds, see https://wiki.hypr.land/Configuring/Basics/Binds/ for more
-hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.exec_cmd("hyprlock"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("command -v hyprctl hyprpaper reload"))
 hl.bind(mainMod .. " + F5", hl.dsp.exec_cmd("killall waybar; sleep 0.2; waybar &"))
+hl.bind(mainMod .. " + SHIFT + T", hl.dsp.exec_cmd("$HOME/.config/waybar/scripts/toggle-theme.sh"))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
